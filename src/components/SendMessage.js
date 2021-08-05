67f1be39d03b27b842bd69142f23454b47d57bfd
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-05 10:12:16 +0530

    loading

---
+++ src/components/SendMessage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SendMessage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SendMessage
---
+++ src/components/SignOut.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignOut() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SignOut
